1a8149662e5e9bb71b44a5ac16fd67bf52e28b48
jim <303600370@qq.com>
2021-06-22 17:18:44 +0800

    update  支持multipart/form-data

--- routerx/router.go
+++ routerx/router.go
@@ -19,31 +19,6 @@ func requestParseJson(fn interface{}) gin.HandlerFunc {
 	}
 	fnt := fnv.Type()
 	return func(c *gin.Context) {
-		//nmIn := fnt.NumIn()
-		//inls := make([]reflect.Value, nmIn)
-		//inls[0] = reflect.ValueOf(c)
-		//for i := 1; i < nmIn; i++ {
-		//	argt := fnt.In(i)
-		//	argtr := argt
-		//	if argt.Kind() == reflect.Ptr {
-		//		argtr = argt.Elem()
-		//	}
-		//	inls[i] = reflect.Zero(argt)
-		//	if strings.Contains(c.ContentType(), "application/json") {
-		//		if argtr.Kind() == reflect.Struct || argtr.Kind() == reflect.Map {
-		//			argv := reflect.New(argtr)
-		//			if err := c.BindJSON(argv.Interface()); err != nil {
-		//				c.String(500, fmt.Sprintf("params err[%d]:%+v", i, err))
-		//				return
-		//			}
-		//			if argt.Kind() == reflect.Ptr {
-		//				inls[i] = argv
-		//			} else {
-		//				inls[i] = argv.Elem()
-		//			}
-		//		}
-		//	}
-		//}
 		inls := bind(c, fnt)
 		defer func() {
 			if err := recover(); err != nil {
@@ -68,6 +43,20 @@ func json(c *gin.Context, argtr reflect.Type) (reflect.Value, error) {
 	return reflect.Value{}, errors.New("argtr not Struct or Map")
 }
 
+func from(c *gin.Context, argtr reflect.Type) (reflect.Value, error) {
+	if argtr.Kind() == reflect.Struct || argtr.Kind() == reflect.Map {
+		argv := reflect.New(argtr)
+		if err := c.Bind(argv.Interface()); err != nil {
+			return argv, err
+		}
+		if argv.Kind() != reflect.Ptr {
+			return argv.Elem(), nil
+		}
+		return argv, nil
+	}
+	return reflect.Value{}, errors.New("argtr not Struct or Map")
+}
+
 func bind(c *gin.Context, fnt reflect.Type) []reflect.Value {
 	nmIn := fnt.NumIn()
 	inls := make([]reflect.Value, nmIn)
@@ -81,6 +70,14 @@ func bind(c *gin.Context, fnt reflect.Type) []reflect.Value {
 		inls[i] = reflect.Zero(argt)
 		if strings.Contains(c.ContentType(), "application/json") {
 			argv, err := json(c, argtr)
+			if err != nil {
+				c.String(500, fmt.Sprintf("params err[%d]:%+v", i, err))
+				return nil
+			}
+			inls[i] = argv
+		}
+		if strings.Contains(c.ContentType(), "multipart/form-data") {
+			argv, err := from(c, argtr)
 			if err != nil {
 				c.String(500, fmt.Sprintf("params err[%d]:%+v", i, err))
 				return nil
