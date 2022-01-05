bb83e038307bdddf0110a8cb4bfee4ddb91e454e
jim <303600370@qq.com>
2022-01-05 21:33:03 +0800

    update : gorountine
    - Recovers
    - 安全的GO

---
+++ routines.go
@@ -0,0 +1,24 @@
+package ginpro
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+func Recover(cleanups ...func()) {
+	for _, cleanup := range cleanups {
+		cleanup()
+	}
+	if p := recover(); p != nil {
+		logrus.Warning(p)
+	}
+}
+
+func Go(fn func()) {
+	if fn == nil {
+		return
+	}
+	go func() {
+		defer Recover()
+		fn()
+	}()
+}
